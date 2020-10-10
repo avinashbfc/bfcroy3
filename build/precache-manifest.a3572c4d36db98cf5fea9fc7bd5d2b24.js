eb858b1f56ab543d54610d8323cebbbb96565477
avinashbcf <avinash.bcfinfotech@gmail.com>
2020-10-10 13:20:17 +0530

    Kisl

---
+++ build/precache-manifest.a3572c4d36db98cf5fea9fc7bd5d2b24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22815596a75f7cd6f1e88875a706627c",
+    "url": "/bfcroy1/index.html"
+  },
+  {
+    "revision": "6f12492d5d144549b754",
+    "url": "/bfcroy1/static/css/main.167e9cae.chunk.css"
+  },
+  {
+    "revision": "cea3848d9d874dc63de0",
+    "url": "/bfcroy1/static/js/2.4d3edbc9.chunk.js"
+  },
+  {
+    "revision": "6e5f0a2beb4752d8dee9f29a3cad8793",
+    "url": "/bfcroy1/static/js/2.4d3edbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f12492d5d144549b754",
+    "url": "/bfcroy1/static/js/main.b7f3829e.chunk.js"
+  },
+  {
+    "revision": "244fd09e00a0cbfea33b",
+    "url": "/bfcroy1/static/js/runtime-main.c7180d90.js"
+  },
+  {
+    "revision": "b3b52c9ed991c8c5f8855010ece74b26",
+    "url": "/bfcroy1/static/media/available.b3b52c9e.png"
+  },
+  {
+    "revision": "ca9064015e4a0256ab92b1166c28c6ce",
+    "url": "/bfcroy1/static/media/feedback_sprite.ca906401.png"
+  },
+  {
+    "revision": "26ca53016c6072535f01ca3f777a8cba",
+    "url": "/bfcroy1/static/media/listbg.26ca5301.png"
+  }
+]);
